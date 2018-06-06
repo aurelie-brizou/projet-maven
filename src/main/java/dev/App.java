0c9620cb03af2286fab890528044193949c910f3
Aurelie Brizou <brizouaurelie@gmail.com>
2018-06-06 12:10:07 +0200

    ajout d'une librarie et suppression des fichiers sts déjà pushés

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,17 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    
+    	String asciiArt = FigletFont.convertOneLine("Hello Aurélie");
+        System.out.println(asciiArt);
+        
     }
 }
